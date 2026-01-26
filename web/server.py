15237154a918481c953dec9343c4ccb24cf92f57
alexcasals <personal@drcasals.com>
2026-01-26 21:18:15 +0100

    ajustes.html nuevo y completo

--- web/server.py
+++ web/server.py
@@ -7,7 +7,7 @@ def run_web_server(shared_state, shared_crono):
 
     # Registrar rutas con dependencias
     register_routes(app, shared_state,shared_crono)
-
+    
     # Arrancar servidor
     app.run(host="0.0.0.0", port=5000, threaded=True,debug=False, use_reloader=False)
 
